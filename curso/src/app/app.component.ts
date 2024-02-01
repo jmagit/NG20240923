d7d4fe9f8991e402f510b97450b52fb44b44120f
Javier <jmartin@grupoloyal.com>
2024-02-01 15:53:38 +0100

    constructor

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from '@my/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Hola mundo';
+
+  constructor(out: LoggerService) {
+    out.error('Es un error')
+    out.warn('Es un warn')
+    out.info('Es un info')
+    out.log('Es un log')
+  }
 }
