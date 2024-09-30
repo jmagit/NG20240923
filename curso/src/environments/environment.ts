20dc5c71019049fe076dfe99585c47418cd7944a
Javier <jmartin@grupoloyal.com>
2024-09-30 12:51:32 +0200

    enableTracing

--- curso/src/environments/environment.ts
+++ curso/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   ERROR_LEVEL: 99,
-  apiURL: '/api/',
-  securityApiURL: '/auth/',
+  apiURL: 'http://localhost:4321/api/',
+  securityApiURL: 'http://localhost:4321/',
 };
 
 /*
